7bc02c1c23ac481df3f46f65ea79cfd3fb01da28
shii-take <shii-take@users.noreply.github.com>
2017-02-01 21:46:20 +0900

    Update do.sh

--- do.sh
+++ do.sh
@@ -13,9 +13,13 @@ brew cask install dropbox
 brew cask install docker
 
 # ruby
-brew install rbenv ruby-build
+brew install rbenv
+echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bash_profile
+echo 'if which rbenv > /dev/null; then eval "$(rbenv init -)"; fi' >> ~/.bash_profile
+brew install ruby-build
 rbenv install 2.2.0
 rbenv global 2.2.0
+source ~/.bash_profile
 gem install bundler
 gem install rubocop
 
