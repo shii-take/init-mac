e1039e4e723343910c77f77be94c23e91a718f74
shii-take <shii-take@users.noreply.github.com>
2017-02-01 21:04:37 +0900

    Create do.sh

---
+++ do.sh
@@ -0,0 +1,20 @@
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# mac util
+brew cask install keepingyouawake
+brew cask install google-japanese-ime
+brew cask install firefox
+brew cask install google-chrome
+brew cask install visual-studio-code
+brew cask install evernote
+brew cask install dropbox
+
+# vm
+brew cask install docker
+
+# ruby
+brew install rbenv ruby-build
+rbenv install 2.1.0
+gem install bundler
+rbenv reahsh
+
